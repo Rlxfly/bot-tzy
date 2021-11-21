3da1864aa381e9ed52c33cc2c9d427130003dd62
Relldev メイ <87053694+Rlxfly@users.noreply.github.com>
2021-11-21 09:01:35 +0700

    Biar brutal

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { itsu, usedPrefix: _p }) => {
 prep = itsu.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 999999999999, status: 1,
-message: 'this bot just recode of script \n\nhttp://github.com/Nurutomo/wabot-aq',
+message: `http://github.com/Rlxfly/bot-tzy\n\nDon't forgot give star and forks this repo:)`,
 orderTitle: 'B',
 sellerJid: '0@s.whatsapp.net',
 thumbnail: global.thumb
