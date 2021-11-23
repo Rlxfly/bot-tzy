110ac484aeaaa6b52b8132618e5dc47025282ea3
Relldev メイ <87053694+Rlxfly@users.noreply.github.com>
2021-11-23 07:00:51 +0700

    Tt

---
+++ plugins/tiktok.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { itsu, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(API('Velgrynd', '/api/tiktok2', { url: args[0] }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let url = json.video_original
+  itsu.sendFile(m.chat, url, 'tiktok.mp4', '', m)
+}
+handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(dl)?)$/i
+
+handler.limit = true
+
+module.exports = handler
