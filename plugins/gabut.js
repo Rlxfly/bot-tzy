4eb97909e6af2157001d1deb60aa25f12ff9ae05
Eʀɪꜱ ` 甘い <87053694+Rlxfly@users.noreply.github.com>
2022-01-29 14:15:44 +0700

    A

--- plugins/gabut.js
+++ plugins/gabut.js
@@ -1,24 +1,21 @@
 let handler = async (m, { itsu, args, command, usedPrefix }) => {
-  if (!args[0]) return m.reply(`Reply video dengan command /${command}`)
+let fe = require('node-fetch')
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? eror : m.sender
+  let pp = 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg'
+  try {
+    pp = await itsu.getProfilePicture(who)
+  } catch (e) {
+
+  }
+	if (!args[0]) return m.reply(`Reply video dengan command /${command}`)
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
-	if (/video/.test(mime))
-	
-	{
-		let vid = m.quoted ? { message: { [m.quoted.mtype]: m.quoted }} : m
-m.reply('Loading')
-		 await itsu.downloadAndSaveMediaMessage(vid, './src/sw').then(() => m.reply('Sending...'))
+	if (/video/.test(mime)) {
+		let vid = await q.download()
+		m.reply('Loading')
+		await itsu.sendMessage(m.chat, vid, 'videoMessage', { quoted: m, duration: args[0] || 9 , thumbnail: await (await fe(pp)).buffer() })
 	} else throw 'Reply videonya!'
-let dur = args[0] || '9'
-let isi = require('fs').readFileSync ('./src/sw.mp4')
-await itsu.sendFile(m.chat, isi, 'pler' + '.mp4', `
- Y
- © ${itsu.user.name}
-`.trim(), m, false, {
-  
-ptt: false, duration: dur, thumbnail: global.thumb3 })
 }
-
 handler.help = ['bugv', 'bugvideo']
 handler.tags = ['tools']
 handler.command = /^bug(v|video)$/i
