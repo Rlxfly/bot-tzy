70510aa51440fc84c9b1b8871432c50c3d73920a
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-10 21:20:42 +0700

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
+  itsu.sendFile(m.chat, json.url, '', 'Nyaa', m)
 }
 handler.help = ['neko']
 handler.tags = ['internet']
